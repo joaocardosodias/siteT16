d50243cc5560e323ce121ba60238cd8b69a076b9
João Cardoso Dias <joao.dias@sou.inteli.edu.br>
2025-04-09 22:26:10 -0300

    Delete docs directory

--- docs/hooks/use-audio.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import { useContext } from "react"
-import { AudioContext } from "@/components/audio-controller"
-
-export function useAudio() {
-  return useContext(AudioContext)
-}
