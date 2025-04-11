76435b3a4d4824c2f1b29728d3402001d49ca846
João Cardoso Dias <joao.dias@sou.inteli.edu.br>
2025-04-10 21:34:21 -0300

    Delete docs directory

--- docs/hooks/use-achievements.tsx
+++
@@ -1,8 +0,0 @@
-"use client"
-
-import { useContext } from "react"
-import { AchievementsContext } from "@/components/achievements-provider"
-
-export function useAchievements() {
-  return useContext(AchievementsContext)
-}
